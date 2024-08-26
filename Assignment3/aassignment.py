5966d021416118c6cbf7c0e52980f0e3410a9ed6
SaurabhGaik.saurabhgaikwadr <SaurabhGaik.saurabhgaikwadr@gmail.com>
2024-08-26 20:41:09 +0530

    Assignment-3

---
+++ Assignment3/aassignment.py
@@ -0,0 +1,73 @@
+#1.Write a program to print the first 10 numbers
+
+# for i in range(1,11):
+#     if (i %2 == 0):
+#         print(i)
+
+
+#2.Write a program to print the first 100 numbers
+
+# for i in range(1,101):
+#     if (i %2 == 0):
+#         print(i)
+
+
+#3.Write a program to print the first ten, 3 digit number
+
+# for i in range(1,10):
+#     if(i % 3 == 0):
+#         print(i)
+
+#4. Write a program to print even numbers 1-100
+
+# for i in range(1,100,):
+#     if (i % 2 == 0):
+#         print(i)
+
+
+#5. Write a program to print odd numbers 1-50
+
+# for i in range(1,50):
+#     if (i %2 != 0):
+#         print(i)
+
+
+
+#6.Write a program to print reverse from 100-1
+
+# for i in range(100,0,-1):
+#     print(i)
+
+
+#7.Write a program to print a table of 12
+
+
+# number = 12
+# for i in range(1, 11):
+#     result = number * i
+# print(f"{number} x {i} = {result}")
+
+
+#8.Write a program to print a table of 12 in reverse order
+
+
+# number = 12
+# for i in range(10,0,-1):
+#     result = number * i
+# print(f"{number} x {i} = {result}")
+
+
+#9.Write a program to print the sum of the first 10 numbers
+
+# sum = 0
+# for num in range(1,11):
+#     sum += num
+# print("sum of number",sum)
+
+
+#10.Write a program to print the product of the first 10 numbers
+
+# product = 1
+# for i in range(1,11):
+#     product *= i
+# print("the prodduct of first 10  number",product)
